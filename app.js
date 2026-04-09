af38288357be53d85a9095ccbcd6a13b8bcbcce0
William Gray Roncal <willgray@gmail.com>
2026-04-08 21:48:23 -0400

    restart

--- app.js
+++
@@ -1,4 +0,0 @@
-import { App } from "./app/components/App.js";
-import { createRoot, html } from "./app/lib.js";
-
-createRoot(document.getElementById("app")).render(html`<${App} />`);
--- rag/__init__.py
+++
@@ -1,5 +0,0 @@
-"""RAG components for grounded Q&A over the learning-engineering dataset."""
-
-from .engine import RAGEngine, RAGResponse
-
-__all__ = ["RAGEngine", "RAGResponse"]
